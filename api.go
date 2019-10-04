494f7718e47940d6a7ed74b8c10221a37e207850
stefba <st@rg-s.com>
2019-10-04 22:47:05 +0200

    Clearify function and introduce dark theme

--- api.go
+++ api.go
@@ -7,24 +7,24 @@ import (
 )
 
 func textApi(w http.ResponseWriter, r *http.Request) {
-	path := r.URL.Path[len("/api/text"):]
 	switch r.Method {
 	case "PUT":
-		if path == "/" {
-			http.Error(w, "must provide filepath", 403)
-			return
-		}
-		body, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			http.Error(w, err.Error(), 500)
-			return
-		}
-		err = ioutil.WriteFile(data + path, body, 0644)
-		if err != nil {
-			http.Error(w, err.Error(), 500)
-			return
-		}
+		err := writeFile(w, r)
+		http.Error(w, err.Error(), 500)
+		return
 	default:
 		fmt.Fprint(w, "GET request")
 	}
+}
+
+func writeFile(w http.ResponseWriter, r *http.Request) error {
+	path := r.URL.Path[len("/api/text"):]
+	if path == "/" {
+		return fmt.Errorf("must provied filepath")
+	}
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return err
+	}
+	return ioutil.WriteFile(data + path, body, 0644)
 }
