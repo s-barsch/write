f0991267b7c1c1abb877b38834e9a32178a3b821
stefba <16940454+stefba@users.noreply.github.com>
2024-05-21 19:00:27 +0200

    Rename Files

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Write from './app';
+import App from './App';
 //import reportWebVitals from './reportWebVitals';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <Write />
+    <App />
   </React.StrictMode>
 );
 
